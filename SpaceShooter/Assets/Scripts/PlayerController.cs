9e4e4ba2598cfaa84823331fb7b78a1b9386c40b
Jordan Selinger <jordanselinger@Jordan-Selingers-MacBook-Pro.local>
2015-09-23 15:45:49 -0600

    Got movement working

---
+++ SpaceShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertizal = Input.GetAxis ("Vertical");
+
+		GetComponent<Rigidbody> ().velocity = new Vector3 (moveHorizontal, 0.0f, moveVertizal) * speed;
+		GetComponent<Rigidbody> ().position = new Vector3
+			(
+				Mathf.Clamp(GetComponent<Rigidbody> ().position.x, xMin, xMax), 
+				0.0f, 
+		    	Mathf.Clamp(GetComponent<Rigidbody> ().position.z, zMin, zMax)
+			);
+	}
+}
